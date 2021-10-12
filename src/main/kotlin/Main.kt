3861c20a22098401b4625c0873bf4a72e498a82e
khulud <Khulud.Saleh.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:52:29 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "khulud"
+    var healthPoints = 100
+    if (healthPoints == 100) {
+        println(name + " is in excellent condition!")
+    } else {
+        println(name + " is in awful condition!")
+    }
+}
